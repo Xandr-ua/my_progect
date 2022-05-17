10b90634a981e6bb3eaca57b4a97b85bbbaa8f19
Alexandr <babiyksasha21@gmail.com>
2022-05-17 19:06:08 +0300

    made a screen header, and made an adaptive to the header

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,8 @@
 $(function() {
 
+  $('.header__burger').on('click', function() {
+    $('.header__list').toggleClass('header__list--active');
+    $('.header__burger').toggleClass('active');
+  });
   
 })
